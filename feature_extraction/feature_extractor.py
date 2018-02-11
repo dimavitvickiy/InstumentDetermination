b1eea823efdbf07c1a5ac2782991a6c28e870309
dima <dimavitvickiy@gmail.com>
2018-02-11 16:07:33 +0200

    add feature extractor

---
+++ feature_extraction/feature_extractor.py
@@ -0,0 +1,17 @@
+from __future__ import print_function
+import librosa
+import librosa.display
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def extract_features(filename):
+    y, sr = librosa.load(filename, sr=44100)
+
+    feature = librosa.feature.melspectrogram(y, sr)
+    plt.figure(figsize=(4, 3))
+    librosa.display.specshow(librosa.power_to_db(feature, ref=np.max),
+                             y_axis='mel', x_axis='time')
+    plt.title('feature')
+    plt.tight_layout()
+    plt.show()
---
+++ main.py
@@ -0,0 +1,5 @@
+from feature_extraction.feature_extractor import extract_features
+
+if __name__ == '__main__':
+    filename = 'guitar_A2_very-long_forte_normal.mp3'
+    extract_features(filename)
