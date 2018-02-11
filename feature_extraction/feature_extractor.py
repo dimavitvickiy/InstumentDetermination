4288266850769cf4d6c4ec69bcbe3e18cf695341
dima <dimavitvickiy@gmail.com>
2018-02-12 00:24:37 +0200

    add features

--- feature_extraction/feature_extractor.py
+++ feature_extraction/feature_extractor.py
@@ -1,17 +1,18 @@
 from __future__ import print_function
 import librosa
 import librosa.display
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 import numpy as np
 
 
 def extract_features(filename):
     y, sr = librosa.load(filename, sr=44100)
 
-    feature = librosa.feature.melspectrogram(y, sr)
-    plt.figure(figsize=(4, 3))
-    librosa.display.specshow(librosa.power_to_db(feature, ref=np.max),
-                             y_axis='mel', x_axis='time')
-    plt.title('melspectrogram')
-    plt.tight_layout()
-    plt.show()
+    feature = librosa.feature.mfcc(y, sr)
+    feature = [np.absolute(arr).mean() for arr in feature]
+    # plt.figure(figsize=(8, 6))
+    # plt.plot(feature)
+    # plt.title(filename.split('_')[0])
+    # plt.tight_layout()
+    # plt.show()
+    return feature
--- main.py
+++ main.py
@@ -1,5 +1,32 @@
+from collections import defaultdict
+
 from feature_extraction.feature_extractor import extract_features
+import csv
+import os
 
 if __name__ == '__main__':
-    filename = 'guitar_A2_very-long_forte_normal.mp3'
-    extract_features(filename)
+    instruments = defaultdict(list)
+    path = os.path.dirname(os.path.realpath(__file__))
+    sample_path = os.path.join(path, 'samples')
+    for (dirpath, dirnames, filenames) in os.walk(sample_path):
+        for instrument in dirnames:
+            instrument_path = os.path.join(sample_path, f'{instrument}')
+            for (dir_, dir_names, filenames_) in os.walk(instrument_path):
+                instruments[instrument].extend(filenames_)
+                break
+        break
+    record_number = sum([len(records) for records in instruments.values()])
+    counter = 0
+    for instrument, records in instruments.items():
+        instrument_path = os.path.join(sample_path, f'{instrument}')
+        for record in records:
+            try:
+                record_path = os.path.join(instrument_path, f'{record}')
+                features = extract_features(record_path)
+                with open('features.csv', 'a', newline='') as csvfile:
+                    writer = csv.writer(csvfile)
+                    writer.writerow(features + [record.split('_')[0]])
+                counter += 1
+                print(f'Read {counter}/{record_number}')
+            except EOFError:
+                continue
